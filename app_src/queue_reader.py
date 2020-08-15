580a640c9bcd30ed28e7e35e48bb8ade9c8cd333
Dave <daves@captricity.com>
2020-08-15 01:53:45 -0400

    incorporated vault into the app worker so we could retrieve api token from vault secret and then call staging using requests

--- app_src/queue_reader.py
+++ app_src/queue_reader.py
@@ -1,6 +1,7 @@
+import os
 import json
-import time
 import dramatiq
+import requests
 from dramatiq.brokers.redis import RedisBroker
 
 
@@ -10,6 +11,29 @@ dramatiq.set_broker(redis_broker)
 
 @dramatiq.actor
 def process_event(event):
-    print('picked up event:  {}'.format(json.loads(event)))
-    time.sleep(5)
-    print('processed event:  {}'.format(json.loads(event)))
+    base_url = 'https://staging.captricity.com/'
+    vault_token = os.environ['VAULT_TOKEN']
+
+    e = json.loads(event)
+    print('picked up event:  {}'.format(e))
+
+    userid = e['userid']
+    event_name = e['name']
+    details = e['details']
+
+    vault_url = 'http://vault:8200/v1/customer_configs/data/config_{}'.format(userid)
+    vault_headers = {'X-Vault-Token': vault_token}
+
+    vault_resp = requests.get(vault_url, headers=vault_headers).json()
+
+    pipeline_config = vault_resp['data']['data']
+
+    if event_name == 'batch-digitized':
+        api_endpoint = 'api/v1/batch/{}'.format(details['batch_id'])
+
+        action = requests.get('{}{}'.format(base_url, api_endpoint),
+                              headers={'Captricity-API-Token': pipeline_config['customer']['shreddr_api_token']})
+
+        print(json.dumps(action.json(), indent=2))
+
+    print('processed event:  {}'.format(e))
